7f923c7bf5b519857672b80a568153fefbaa9227
syed asim <aasimshah8110@gmail.com>
2023-02-11 22:04:36 +0500

    adfadf

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 const  userModel = require('./models/userModel')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 const path = require('path')
@@ -14,22 +16,37 @@ app.use('/' , express.static(__dirname + '/public'))
 // mongoose.connect(process.env.DB_URL_LIVE).then(res=>console.log('compass db connected')).catch(err => console.log(err))
  mongoose.connect("mongodb+srv://asim:mardan@cluster0.btwlh.mongodb.net/fatimahh?retryWrites=true&w=majority").then(res=>console.log('atlass db connecteed')).catch(err => console.log(err))
 
-
+ app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set("view engine" , "ejs")
+app.use(cookieParser())
+
+
+const checkUser = async(req ,res , next) =>{
+  const userId = req.cookies.userId
+  console.log(userId)
+  if(userId){
+    const user = await userModel.findById(userId)
+    return res.render('Homepage' , { user : user})
+  }
+  next()
+}
 
 
 
 
 
 
-
-
-
-app.get("/"   , async(req ,res) =>{
-  res.render('Homepage')
+app.get("/" , checkUser,async(req ,res) =>{
+  console.log(req.cookies)
+  res.render('Homepage' , {user: null})
 })
 
 
@@ -93,8 +110,9 @@ app.post('/login', async(req , res , done)=>{
 const {email , password} = req.body
 const user = await userModel.findOne({email})
 if (!user) { return res.render("LoginPage" , {err : "No User Found"})}
-if (! await bcrypt.compare(password ,user.password)) { return res.render("LoginPage" , {err : "No User Found"})}
-res.render('Homepage')
+if (! await bcrypt.compare(password ,user.password)) { return res.render("LoginPage" , {err : "Wrong Credintials"})}
+res.cookie('userId'  , user._id)
+res.render('Homepage' , {user: user})
 });
 
 
